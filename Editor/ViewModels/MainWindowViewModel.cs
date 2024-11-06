5e70059ea5932337d906d964d351615124378d3e
Evangelion Manuhutu <evangelion11.m@gmail.com>
2024-11-06 19:59:16 +0700

    Tree View Test

--- Editor/ViewModels/MainWindowViewModel.cs
+++ Editor/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,23 @@
-﻿namespace Editor.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Editor.Models;
+
+namespace Editor.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public ObservableCollection<Node> MyNodes { get; set; }
+    
+    public MainWindowViewModel()
+    {
+        MyNodes = new ObservableCollection<Node>
+        {
+            new Node("Animals", new ObservableCollection<Node>
+            {
+                new Node("Mammals", new ObservableCollection<Node>
+                {
+                    new Node("Lion"), new Node("Cat"), new Node("Zebra")
+                })
+            })
+        };
+    }
 }
--- Editor/Views/MainWindow.axaml.cs
+++ Editor/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Avalonia.Controls;
